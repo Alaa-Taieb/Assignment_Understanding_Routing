6bb7c8b176aab48709ccdee2dff786380ce5c61a
Alaa Taieb <alaataieb.tn@gmail.com>
2023-02-21 16:51:03 +0100

    Finished Assignment

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<string:message>')
+def say(message):
+    return f"Hi {message}!"
+
+@app.route('/repeat/<int:number>/<string:message>')
+def repeat(number, message):
+    return message * int(number) 
+
+@app.errorhandler(404)
+def not_found_handler(e):
+    return "Sorry! No response. Try again."
+if __name__:
+    app.run(debug = True, port = 5000)
+
